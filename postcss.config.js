6166ac844cbfbc76b0d6071cea88d44902f89f59
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-04-01 23:12:50 -0500

    finiquitando

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
-import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
-
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@": "/resources/js",
-        },
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
     },
-});
+};
