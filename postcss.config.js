d71f7232f18cda6aa9467afd4e2f534c57349d4e
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-04-01 23:01:34 -0500

    instalacion npm

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": "/resources/js",
+        },
     },
-};
+});
